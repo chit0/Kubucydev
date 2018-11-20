7c8a99e5e6d369b5fe3cf5aecbcc75ff3ca33213
Cyto <39250170+chit0@users.noreply.github.com>
2018-11-20 15:27:35 +0700

    Add files via upload

---
+++ inikah.rb
@@ -0,0 +1,7 @@
+print "berapa bahasa yang harus dikuasai stah "
+N = gets.chomp.to_i
+puts "jadi harus #{N} bahasa program yang harus dikuasai"
+print "salken cyto nama stah siapa "
+N1 = gets.chomp
+puts "makasih infonya mastah #{N1}"
+ 
